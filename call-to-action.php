36acd635d808933f8823625695d54f47f1a741d6
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-05-03 15:44:02 -0500

    changes

--- call-to-action.php
+++ call-to-action.php
@@ -2,6 +2,6 @@
     <h2 data-aos="fade-down-right" data-aos-easing="linear" data-aos-duration="900">Uncover the Enchantment of Learning at Candyland School</h2>
     <p data-aos="fade-down-left" data-aos-easing="linear" data-aos-duration="1000">Enroll your child today and give them the gift of a world-class education in a fun and stimulating environment.</p>
     <button class="enroll-btn-" data-aos="zoom-in" data-aos-easing="linear" data-aos-duration="1500">
-        <a href="contact.php" style="color:#fff; text-decoration:none;">Contact Us</a>
+        <a href="contact.php" style="color:#fff; text-decoration:none;">Contact</a>
     </button>
 </section>
